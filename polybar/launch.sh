7275fcaeb9482b217cc9e57e9aed1fe12b1c7e23
João Vivas <filipe.vivas.fv@gmail.com>
2023-08-07 22:47:25 +0100

    Added support to virtual monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,6 @@ polybar-msg cmd quit
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log 
 polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
