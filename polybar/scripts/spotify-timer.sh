1f227efce1366e6180436c52674a563cac80c671
João Vivas <filipe.vivas.fv@gmail.com>
2023-08-16 19:48:49 +0100

    altered bar

--- polybar/scripts/spotify-timer.sh
+++ polybar/scripts/spotify-timer.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#while true
-#do
-#  length=$(playerctl metadata --format "{{duration(mpris:length)}}")
-#  position=$(playerctl metadata --format "{{duration(position)}}")
-#  echo "$position - $length"
-#  sleep 1s
-#done
 length=$(playerctl metadata --format "{{duration(mpris:length)}}")
 position=$(playerctl metadata --format "{{duration(position)}}")
 echo "($position | $length)"
