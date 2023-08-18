100e88d22bd7976faa0c38e36c30e003ca5c6215
MicoMan <filipe.vivas.fv@gmail.com>
2023-08-18 18:16:47 +0100

    moved the location of Deskcreen in my mon2 script

--- scripts/mon2.sh
+++ scripts/mon2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Script to add a virtual monitor and turn on Deskreen
 xrandr --output VIRTUAL1 --mode 1366x768 --right-of eDP1
 
 sleep 3
 i3-msg restart
-~/Downloads/Deskreen-2.0.4.AppImage
+~/Deskreen-2.0.4.AppImage
 
