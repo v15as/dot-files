182586b8617a1433c34a9c2d368b97311c4232c7
MicoMan <filipe.vivas.fv@gmail.com>
2023-08-23 11:37:36 +0100

    Corrected audio script

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -5,5 +5,5 @@ volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk 'NR==1{print $5}' | sed 's/\
 
 
 if [ $volume -lt 100 ]; then
-  $(pactl set-sink-volume @DEFAULT_SINK@ +10%)
+  $(pactl set-sink-volume @DEFAULT_SINK@ +1%)
 fi
